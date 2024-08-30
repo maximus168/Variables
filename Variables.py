17cee07d2068fa50aecc617af4fdae49d090919a
maximus168 <mikula-77@mail.ru>
2024-08-30 21:25:56 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,7 @@
+DZ=12
+Hours=1.5
+Cours='Python'
+Speed=Hours/DZ
+print(Speed)
+print(DZ,Hours,Cours)
+print('Курс: ',Cours,', всего задач: ',DZ,', затрачено часов: ',Hours,', среднее время выполнения ',Speed)
